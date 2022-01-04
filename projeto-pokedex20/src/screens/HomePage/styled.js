848c87bbf9d27dbb9fd1cc4b33a1aed910970fe2
José Rodolfo Valério Costa <rdfvalerio@gmail.com>
2022-01-04 17:25:44 -0300

    Construção inicial dos componentes Home-Pokedex

---
+++ projeto-pokedex20/src/screens/HomePage/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ContainerHome = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`
+export const ContainerCards = styled.div`
+  width: 98%;
+  flex-wrap: wrap;
+  display: flex;
+  justify-content: space-around;
+`
